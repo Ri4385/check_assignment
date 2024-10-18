669dcc0ebfc4f971010182d50a6bcd0ed4569a2d
Ri4385 <tkd1121g@gmail.com>
2024-10-19 07:14:24 +0900

    atreamlitのために修正

--- src/config.py
+++ src/config.py
@@ -1,14 +1,15 @@
 from pydantic_settings import BaseSettings
-
+import streamlit as st
 
 class Settings(BaseSettings):
-    debug: bool
-    username: str
-    password: str
-    login_url: str
+    # debug: bool
+    # username: str
+    # password: str
+    login_url: str = st.secrets['.env']['LOGIN_URL']
 
-    class Config:
-        env_file = ".env"
+    # class Config:
+    #     # env_file = ".env"
+    #     env_file = ".env"
 
 
 setting = Settings()  # pyright: ignore
