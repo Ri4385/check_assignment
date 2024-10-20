bc690f8356cc83053138f3fd352992cf7cb01828
Ri4385 <tkd1121g@gmail.com>
2024-10-21 04:45:08 +0900

    UI

--- src/main.py
+++ src/main.py
@@ -50,14 +50,7 @@ class AssignmentCard(BaseModel):
     def display(self) -> None:
 
         card = ""
-
-        card += '<style>'
-        card += '@media screen and (max-width: 768px) {'
-        card += 'div { width: 70vw; }'
-        card += '}'
-        card += '</style>'
-
-        card += '<div style="border: 1px solid #ccc; border-radius: 15px; padding: 10px; margin: 5px;">'
+        card += '<div style="border: 1px solid #ccc; border-radius: 15px; padding: 10px; margin: 5px; width:70vw">'
         card += f'<h6>{self.title}</h6>'
         card += f'<p>提出期限 : {self.duetime}</p>'
         if self.due:
