bad4a748d6c1a1834893a79b5027c31dee77d894
Ri4385 <tkd1121g@gmail.com>
2024-10-22 23:17:57 +0900

    release_info修正

--- src/gui/release_info.py
+++ src/gui/release_info.py
@@ -8,12 +8,13 @@ def main() -> None:
     st.write("2024/10/22: 授業資料を新しいものから見れるようにしました")
     st.write("")
 
-    st.write("<br><br>", unsafe_allow_html=True)
+    st.write("<br>", unsafe_allow_html=True)
 
     st.write("#### planning for updates")
     st.write("PandAのログインスキップ")
     st.write("授業ごとに授業資料を見れるようにする")
     st.write("課題が提出期限の早いものから表示")
+    st.write("課題の取得時間を書きたい")
     st.write("")
 
     return
