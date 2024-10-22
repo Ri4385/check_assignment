fb087926e4d0393ceb28596c94a31e764b701a5d
Ri4385 <tkd1121g@gmail.com>
2024-10-22 22:56:42 +0900

    授業資料を見れるように

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from api import login
 from gui import assignment_viewer
 from gui import resources_viewer
+from gui import release_info
 
 
 def main() -> None:
@@ -12,12 +13,12 @@ def main() -> None:
         page_title="PandA for the Lazy",
         layout="wide",
     )
-    
+
     # Streamlitのタイトル
     st.title("PandA for the Lazy")
 
     # ログインセクション
-    if 'logged_in' not in st.session_state:
+    if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
     if not st.session_state.logged_in:
@@ -27,24 +28,28 @@ def main() -> None:
 
         if st.button("Login"):
             if username and password:
-                session = login.login_with_password(username=username, password=password)
+                session = login.login_with_password(
+                    username=username, password=password
+                )
                 st.session_state.session = session  # セッションを保存
                 st.session_state.logged_in = True  # ログイン状態を更新
                 st.success("Logged in successfully!")
-                time.sleep(1.2)
+                time.sleep(0.8)
                 st.rerun()
             else:
                 st.warning("Please enter both username and password.")
     else:
-        tab_assignment, tab_resources = st.tabs(["assignments", "resources"])
+        tab_assignment, tab_resources, tab_releases = st.tabs(
+            ["assignments", "resources", "releases"]
+        )
 
         with tab_assignment:
             assignment_viewer.main()
         with tab_resources:
             resources_viewer.main()
+        with tab_releases:
+            release_info.main()
 
-    
 
-        
 if __name__ == "__main__":
     main()
--- src/gui/resources_viewer.py
+++ src/gui/resources_viewer.py
@@ -2,22 +2,42 @@ from datetime import datetime
 import time
 
 import streamlit as st
+from pydantic import BaseModel
 
 from api import client
 from api.model import Course
 from api.model import Resource
+
+
+class ResourceCard(BaseModel):
+    title: str
+    name: str
+    modified: int
+    url: str
+
+    def display(self) -> None:
+        card = ""
+        card += '<div style="border: 1px solid #ccc; border-radius: 15px; padding: 10px; margin: 10px;">'
+        card += f"<h6>{self.title}</h6>"
+        card += f'<a href="{self.url}" target="_blank">{self.name}</a>'
+        card += "</div>"
+
+        st.markdown(card, unsafe_allow_html=True)
+        return
+
 
 def get_year_and_semester() -> tuple[str, str]:
     current_date = datetime.now()
     year_int: int = current_date.year
     if current_date.month < 4:
         year_int -= 1
-    year:str = str(year_int)
+    year: str = str(year_int)
     if 4 <= current_date.month < 9:
         semester = "前期"
     else:
         semester = "後期"
     return year, semester
+
 
 def skip_request(course: Course, year: str, semester: str) -> bool:
     if course.title == "Home":
@@ -31,33 +51,47 @@ def skip_request(course: Course, year: str, semester: str) -> bool:
         return True
     return False
 
+
 def main() -> None:
-    st.write("### Resources Viewer")
+    st.write("## Resources Viewer")
 
     start = time.perf_counter()
 
     if st.button("Get Resources"):
         courses: list[Course] = client.get_cources(session=st.session_state.session)
-        
+
         year, semester = get_year_and_semester()
 
-        resources: list[Resource] = []
+        resource_cards: list[ResourceCard] = []
         for course in courses:
             if skip_request(course=course, year=year, semester=semester):
                 continue
-            st.write(course.title)
-            res = client.get_resources_by_api(session=st.session_state.session, id=course.id)
-            resources.extend(res)
-            for res in resources:
-                st.write(res.name)
-                # st.write(res.url)
+            resources = client.get_resources_by_api(
+                session=st.session_state.session, id=course.id
+            )
+
+            for resource in resources:
+                resource_card = ResourceCard(
+                    title=course.title,
+                    name=resource.name,
+                    url=resource.url,
+                    modified=resource.modified,
+                )
+                resource_cards.append(resource_card)
+
+        resource_cards.sort(key=lambda res: -res.modified)
+
+        for resource_card in resource_cards:
+            resource_card.display()
+
         st.write(time.perf_counter() - start, "s")
-        
+
         # resources.sort(key=lambda res: -res.modified)
         # for res in resources:
         #     st.write(res.name, res.modified)
         #     st.write(res.url)
     return
+
 
 if __name__ == "__main__":
     main()
