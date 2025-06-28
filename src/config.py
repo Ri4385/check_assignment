faf85a3c0e03beb0914ddf7eb86910d918a84488
Ri4385 <tkd1121g@gmail.com>
2025-06-28 19:19:56 +0900

    環境変数をsecrets.tomlに移行

--- src/config.py
+++ src/config.py
@@ -1,15 +1,13 @@
 from pydantic_settings import BaseSettings
+import streamlit as st
 
 
 class Settings(BaseSettings):
     debug: bool
     login_url: str
 
-    class Config:
-        env_file = ".env"
 
-
-setting = Settings()  # pyright: ignore
+setting = Settings(**st.secrets)
 
 if __name__ == "__main__":
     print(setting)
